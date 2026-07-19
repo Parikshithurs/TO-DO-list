cf8a6d9b6f91cc358fac7831e18eeacbfdba56fd
Parikshithurs <parirkshithursparikshithurs@gmail.com>
2026-07-19 11:40:58 +0530

    credentials for database connection

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+import psycopg
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_connection():
+    return psycopg.connect(
+        host=os.getenv("DATABASE_HOST"),
+        port=os.getenv("DATABASE_PORT"),
+        dbname=os.getenv("DATABASE_NAME"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD")
+    )
